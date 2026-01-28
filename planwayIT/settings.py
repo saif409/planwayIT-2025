169afc34b779c147885d68de46f2393d2157fce8
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2026-01-28 18:29:17 +0600

    feat: static
    Signed-off-by: Nahidujjaman Hridoy <hridoyboss12@gmail.com>

--- planwayIT/settings.py
+++ planwayIT/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -127,6 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
